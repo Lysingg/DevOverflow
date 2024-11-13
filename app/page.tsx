a2b1d39c279d83775bafccb257fa16418b146b10
Lysingg <lysing0000@gmail.com>
2024-11-13 10:19:18 +0700

    create next.js application

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <h1 className=" font-space-grotesk font-bold text-dark-100">
+        HELLO WORLD
+      </h1>
+      <h1 className=" font-inter font-bold text-dark-100">HELLO WORLD</h1>
+    </>
+  );
+};
+
+export default page;
