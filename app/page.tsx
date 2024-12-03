9eb36b3b2063a98d4e3655dc13a36cce4f58afbf
Lysingg <lysing0000@gmail.com>
2024-12-03 17:17:44 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <h1 className=" font-space-grotesk font-bold text-dark-100">
-        HELLO WORLD
-      </h1>
-      <h1 className=" font-inter font-bold text-dark-100">HELLO WORLD</h1>
+      <h1 className=" font-space-grotesk font-bold text-white">HELLO WORLD</h1>
+      <h1 className=" font-inter font-bold text-white">HELLO WORLD</h1>
     </>
   );
 };
